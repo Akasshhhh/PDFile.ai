db0f49b9a7ef95710658fd272c4f2a6f53a58a36
Akasshhhh <akashisntreal@gmail.com>
2025-04-02 13:44:38 +0530

    Updated langchain and its imports

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,6 +1,6 @@
 import {Pinecone, PineconeRecord} from "@pinecone-database/pinecone"
 import { downloadFromS3 } from "./s3-server";
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { Document, RecursiveCharacterTextSplitter} from "@pinecone-database/doc-splitter"
 import { getEmbeddings } from "./embeddings";
 import md5 from "md5"
